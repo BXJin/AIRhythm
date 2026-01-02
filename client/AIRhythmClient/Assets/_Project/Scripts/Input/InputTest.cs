63798dd14a3cdb8d9c5033306f73fd361b36a27e
BXJin <bsung472@gmail.com>
2026-01-02 17:24:18 +0900

    Input 테스트

---
+++ client/AIRhythmClient/Assets/_Project/Scripts/Input/InputTest.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    private int hitCount = 0;
+
+    public void OnHit()
+    {
+        Debug.Log($"Hit{hitCount++} detected!");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
